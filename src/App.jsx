114883609724e930abb58e6ed6e5efd100f7d9fc
Dijitalex <alex.dong.reno@gmail.com>
2026-04-21 21:28:24 -0700

    X's and O's can now be added to the grid without replacement

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
+import { useState } from 'react'
 import * as React from 'react'
 import * as ReactBootstrap from 'react-bootstrap'
 
-const { Badge, Button, Card } = ReactBootstrap
+function Square({value, onSquareClick}){
 
-function Square({value}){
-  function handleClick(){
-    console.log("Clicked!")
-  }
+
+  // function handleClick(){
+  //   setValue('X');
+  // }
   return(
-    <button className="square" onClick={handleClick}></button>
-  )
+    <button
+      className="square" onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
-  return <>
-  <div className = "board-row">
-    <Square value="1" />
-    <Square value="2"/>
-    <Square value="3"/>
-  </div>
-  <div className = "board-row">
-    <Square value="3"/>
-    <Square value="3"/>
-    <Square value="3"/>
-  </div>
-  <div className = "board-row">
-    <Square value="3"/>
-    <Square value="3"/>
-    <Square value="3"/>
-  </div>
-  </>;
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    if(squares[i]){
+      return;
+    }
+    if(xIsNext){
+      nextSquares[i] = "X";
+    }
+    else{
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
 }
